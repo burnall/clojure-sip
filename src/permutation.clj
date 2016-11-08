670033466fee955fe666bd6073cc99695a2aca62
Maxim Fainberg <maksym.fainberg@mobenga.com>
2016-11-08 13:12:21 +0100

    Add libs, back to Squares Squared

--- src/permutation.clj
+++
@@ -1,5 +0,0 @@
-(ns permutation)
-
-(defn is-permutation [a b]
-  (println 444)
-  )
--- src/puz6.clj
+++ src/puz6.clj
@@ -7,7 +7,7 @@
   (let
     [s (apply str (take-while #(<= % b) (iterate #(* % %) a)))
      l (count s)
-     n (Math/ceil (Math/sqrt l))
+     n (int (Math/ceil (Math/sqrt l)))
      t (vec (str s (apply str (repeat (- (* n n) l) \*))))
      f (fn[a i j v] (assoc a i (assoc (a i) j v)))
      ma (#(repeat % (repeat % \space)) (dec (* 2 n)))
@@ -16,10 +16,10 @@
   ))
 
 
-(ss 4 20)
+(println (ss 4 20))
 
 
-; graph 1
-(defn g[s]
-  (println (clojure.string/split s "\d+"))
-  )
+;; graph 1
+;(defn g[s]
+;  (println (clojure.string/split s "\d+"))
+;  )
