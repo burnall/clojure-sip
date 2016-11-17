035c90944e60050871481bb1e89d6e4fa7f2f03c
Maxim Fainberg <maksym.fainberg@mobenga.com>
2016-11-17 12:43:16 +0100

    Squares Sqaured - done

--- src/puz6.clj
+++ src/puz6.clj
@@ -10,26 +10,27 @@
      n (int (Math/ceil (Math/sqrt l)))
      t (vec (str s (apply str (repeat (- (* n n) l) \*))))
      f (fn [a i j v] (assoc a i (assoc (a i) j v)))
-     ma (#(repeat % (repeat % \space)) (dec (* 2 n)))
+     ma (vec (#(repeat % (vec (repeat % \space))) (dec (* 2 n))))
      d [[1 1] [-1 1] [-1 -1] [1 -1]]
-     mid (+ n -2 (mod n 2))
-     ]
-    (reduce (fn [[[a b] ps grad step sc fst] i]
-              (let [[grad1 step1 sc1 fst1]
-                    (cond (< step sc) [grad (inc step) sc fst]
-                          fst [(inc grad) 1 sc false]
-                          :else [(inc grad) 1 (inc sc) true])
-                    [dx dy] (d (mod grad1 4))]
-                [[(+ a dx) (+ b dy)] (conj ps [a b i]) grad1 step1 sc1 fst1]))
-            [[mid mid] [] 0 0 1 true] t)
+     mid [(dec n) (+ n -2 (mod n 2))]
+     ps (first (reduce (fn [[ps [a b] grad step sc fst] i]
+                         (let [[grad1 step1 sc1 fst1]
+                               (cond (< step sc) [grad (inc step) sc fst]
+                                     fst [(inc grad) 1 sc false]
+                                     :else [(inc grad) 1 (inc sc) true])
+                               [dx dy] (d (mod grad1 4))]
+                           [(conj ps [a b i]) [(+ a dx) (+ b dy)] grad1 step1 sc1 fst1]))
+                       [[] mid 0 0 1 true] t))
+     ] (map (partial apply str) (reduce (fn[m [i j v]] (f m j i v)) ma ps))
     )
   )
 
 
-(println (ss 2 256))
+(map println (ss 5 5000))
 
 
 ;; graph 1
 ;(defn g[s]
 ;  (println (clojure.string/split s "\d+"))
 ;  )
+
