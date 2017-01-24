783813329137525cfd9e93d03ad95fa616c0a36c
Maxim Fainberg <maksym.fainberg@mobenga.com>
2017-01-24 11:19:31 +0100

    Back to business! Plan for latin square

---
+++ src/latin_square.clj
@@ -0,0 +1,8 @@
+(ns latin-square)
+
+; get max row's width
+; get all combinations
+; seek squares in every combination
+;  try every square of size 2, 3, ... with left top in the specific point
+; distinct and count by size
+(defn count-ls[s])
