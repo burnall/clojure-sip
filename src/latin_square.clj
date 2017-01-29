dbdcb176a846a7284b88546fbef0a1dc42bb38f1
Maxim Fainberg <maksym.fainberg@mobenga.com>
2017-01-29 23:21:27 +0100

    Latin square, approaching

--- src/latin_square.clj
+++ src/latin_square.clj
@@ -5,4 +5,17 @@
 ; seek squares in every combination
 ;  try every square of size 2, 3, ... with left top in the specific point
 ; distinct and count by size
-(defn count-ls[s])
+(defn count-ls [s]
+  (let [
+        maxWidth (apply max (map count s))
+        allMoves (fn [v n]
+                   (map #(vec (concat (repeat % 0) v (repeat (- n % (count v)) 0)))
+                        (range (- (inc n) (count v)))))
+        product (fn [vs v] (for [va vs i v] (conj va i)))
+        allCombinations (reduce product [[]] (map #(allMoves % maxWidth) s))
+        ]
+    allCombinations
+  ))
+
+;(println (count-ls [[1 2 3] [2 3 1 2 1] [3 1 2]]))
+(println (count-ls [[1 2 3] [:a]]))
