ca81afa60cbccdc2d29145ac542744cfd8862334
Maxim Fainberg <maksym.fainberg@mobenga.com>
2018-08-27 22:32:19 +0200

    Veitch - code style

--- src/sip/veitch2.clj
+++ src/sip/veitch2.clj
@@ -1,118 +1,97 @@
 (ns sip.veitch2)
 
 ;#{'a 'B 'C 'd} => [0110]
-(defn toIntArray [set] 
+(defn to-int-array [set] 
   (mapv (comp #(if (some? %) 1 0) set) 
         (take (count set) ['A 'B 'C 'D])))
 
 ;[0110] => #{'a 'B 'C 'd}
-(defn toLetterSet [bits]
+(defn to-letter-set [bits]
   (set
-    (mapcat 
-      (fn [bit t f] (condp = bit 0 [f] 1 [t] 2 []))
-      bits 
-      ['A 'B 'C 'D] 
-      ['a 'b 'c 'd])))
+   (mapcat (fn [bit t f] (condp = bit 0 [f] 1 [t] 2 []))
+           bits 
+           ['A 'B 'C 'D] 
+           ['a 'b 'c 'd])))
 
 ;[0 1 1 0] [0 1 0 0] => {:diff 1 :res [0 1 2 0]}
-(defn _glue [termA termB]
+(defn _glue [term-a term-b]
   (reduce (fn [{diff :diff res :res} [a b]] 
              (if (= a b)
-                {:diff diff :res (conj res a)}
-                {:diff (inc diff) :res (conj res 2)}))
-          {:diff 0 :res []}
-          (map vector termA termB)))  
+                {:diff diff, :res (conj res a)}
+                {:diff (inc diff), :res (conj res 2)}))
+          {:diff 0, :res []}
+          (map vector term-a term-b)))  
 
-(defn glue[termA termB]
-  (let [g (_glue termA termB)]
+(defn glue[term-a term-b]
+  (let [g (_glue term-a term-b)]
     (when (= (:diff g) 1) 
           (:res g))))    
 
 (defn match[terms]
-  (for [termA terms 
-        termB terms
-        :when (< (:id termA) (:id termB))
-        :let [g (glue (:data termA) (:data termB))]
+  (for [term-a terms 
+        term-b terms
+        :when (< (:id term-a) (:id term-b))
+        :let [g (glue (:data term-a) (:data term-b))]
         :when (some? g)]
-    {:data g :parents [(:id termA) (:id termB)]}))
+    {:data g, :parents [(:id term-a) (:id term-b)]}))
 
-(defn addId[terms] 
+(defn add-id[terms] 
   (mapv (fn[id term] (assoc term :id id))
-       (range) 
-       terms))
+        (range) 
+        terms))
 
-(defn buildSolution[solutionInProgress]
-  (if-let [next (seq (match (last solutionInProgress)))]
-    (recur (conj solutionInProgress (addId next))) 
-    solutionInProgress))
+(defn build-solution[solution-in-progress]
+  (if-let [next (seq (match (last solution-in-progress)))]
+    (recur (conj solution-in-progress (add-id next))) 
+    solution-in-progress))
 
 ;{:data [1 0 2 1], :parents [0 5], :id 1}
-(defn isReducible[solution level id]
+(defn reducible?[solution level id]
   (and (< (inc level) (count solution))
        (some (fn [{parents :parents}] (some #(= id %) parents)) 
              (solution (inc level)))))
 
 ;[{:term term, :covers [id1...]}]
-(defn findIrreducibles [solution]
+(defn find-irreducibles [solution]
   (set
-    (mapcat
-      (fn [level] 
-        (map :data 
-          (filter #(not (isReducible solution level (:id %)))
-                  (solution level))))
-      (range 0 (count solution)))))
+   (mapcat (fn [level] 
+             (map :data 
+                  (filter #(not (reducible? solution level (:id %)))
+                          (solution level))))
+           (range 0 (count solution)))))
 
 (defn covers? [generic term]
   (every? (fn [[a b]] (or (= a 2) (= a b)))
           (map vector generic term)))
 
+(defn find-min-coverage [original-terms derived-terms]
+  (let [get-derived-coverage-for-term 
+         (fn [term] (set (filter (fn [derived] (covers? derived term)) derived-terms)))
 
-(defn findMinCoverage [originalTerms derivedTerms]
-  (:minTerms
-    (reduce (fn [{notCoveredTerms :notCoveredTerms minTerms :minTerms} derivedTerm]
-              (if-let [covered (seq (filter #(covers? derivedTerm %) notCoveredTerms))]
-                  {:notCoveredTerms (clojure.set/difference notCoveredTerms covered)
-                   :minTerms (conj minTerms derivedTerm)}
-                  {:notCoveredTerms notCoveredTerms :minTerms minTerms}))
+        cover-map (into {} (mapv (fn[term] [term (get-derived-coverage-for-term term)]) original-terms))
 
-            {:notCoveredTerms originalTerms
-             :minTerms #{}}
+        get-min-of-two (fn [[term-a cover-a] [term-b cover-b]]
+                       (if (< (count cover-b) (count cover-a)) [term-b cover-b] [term-a cover-a]))
+        find-min (fn [cover-map] (reduce get-min-of-two cover-map))
 
-             derivedTerms)))
+        remove-derived (fn [cover-map derived-term]
+          (into {} (filter (fn [[term cover]] (not (contains? cover derived-term))) cover-map)))
 
-(defn findMinCoverage2 [originalTerms derivedTerms]
-  (let [getDerivedCoverageForTerm 
-         (fn [term] (set (filter (fn [derived] (covers? derived term)) derivedTerms)))
-
-        coverMap (into {} (mapv (fn[term] [term (getDerivedCoverageForTerm term)]) originalTerms))
-
-        getMinOfTwo (fn [[termA coverA] [termB coverB]]
-                       (if (< (count coverB) (count coverA)) [termB coverB] [termA coverA]))
-        findMin (fn [coverMap] (reduce getMinOfTwo coverMap))
-
-        removeDerived (fn [coverMap derivedTerm]
-          (into {} (filter (fn [[term cover]] (not (contains? cover derivedTerm))) coverMap)))
-
-        _findMinCoverage 
-          (fn [acc coverMap] 
-            (let [[term cover] (findMin coverMap) 
-                 derived (first cover)
-                 newCoverMap (removeDerived coverMap derived)
-                 newAcc (conj acc derived)]
-              (if (zero? (count newCoverMap))
-                newAcc
-                (recur newAcc newCoverMap))))    
-
-        ]
-    (_findMinCoverage #{} coverMap)))   
+        _find-min-coverage 
+          (fn [acc cover-map] 
+            (let [[term cover] (find-min cover-map) 
+                  derived (first cover)
+                  new-cover-map (remove-derived cover-map derived)
+                  new-acc (conj acc derived)]
+              (if (zero? (count new-cover-map))
+                new-acc
+                (recur new-acc new-cover-map))))]
+    (_find-min-coverage #{} cover-map)))   
      
-(defn vei [termSet]
-  (let [originalTerms (set (map toIntArray termSet))
-        terms (addId (mapv (fn[term] {:data term}) originalTerms))
-        sol (buildSolution [terms])
-        irr (findIrreducibles sol)
-        minTerms (findMinCoverage2 originalTerms irr)]
-    (set (map toLetterSet minTerms)))) 
-
-                                                                        
-
+(defn vei [term-set]
+  (let [original-terms (set (map to-int-array term-set))
+        terms (add-id (mapv (fn[term] {:data term}) original-terms))
+        sol (build-solution [terms])
+        irr (find-irreducibles sol)
+        min-terms (find-min-coverage original-terms irr)]
+    (set (map to-letter-set min-terms)))) 
