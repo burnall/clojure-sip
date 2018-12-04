c3f248aa410dd289c6084b37acc523351c03ca7e
Maxim Fainberg <maksym.fainberg@mobenga.com>
2018-12-04 08:38:46 +0100

    Advent of code - day 2

--- src/sip/advent_of_code_2019.clj
+++ src/sip/advent_of_code_2019.clj
@@ -24,4 +24,48 @@
          (reductions + 0)
          (find-non-unique #{})
          (take 10))))
-         
+
+(def input2
+  (-> "src/sip/adv-input2.txt"
+      (slurp)
+      (clojure.string/split #"\n")))
+
+(defn get-checksum-values [line]
+  (letfn [(has-value [m v] (some (partial = v) (vals m)))
+          (f [b] (if b 1 0))] 
+    (-> line
+        (frequencies)
+        ((fn [m] [(f (has-value m 2)) 
+                  (f (has-value m 3))])))))
+     
+(defn adv3 [] 
+  (->> input2
+       (map get-checksum-values)
+       ((fn [checksums] (* (reduce + (map first checksums)) (reduce + (map second checksums))))))) 
+
+(defn diff-lists[xa xb] 
+  (reduce (fn [acc [a b]] 
+            (if (= a b) 
+              acc
+              (if (zero? acc) 1 (reduced 2)))) 
+          0
+          (map vector xa xb)))
+
+(defn find-diff-1 []
+  (for [i (range (count input2))
+        j (range i (count input2))
+        :when (= 1 (diff-lists (input2 i) (input2 j)))]
+    [i j]))
+
+(defn extract-similarity [xa xb]
+  (reduce (fn [acc [a b]] (if (= a b) (conj acc a) acc))
+          []
+          (map vector xa xb)))
+
+(defn adv4[] 
+  (->> (find-diff-1)
+       (first)
+       (map input2)
+       (apply extract-similarity)
+       (apply str))) 
+  
