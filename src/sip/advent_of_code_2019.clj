304785914ad9985b38c9bc1f5543f7e24b1a86b3
Maxim Fainberg <maksym.fainberg@mobenga.com>
2018-12-05 15:43:19 +0100

    Advent of code - day 3

--- src/sip/advent_of_code_2019.clj
+++ src/sip/advent_of_code_2019.clj
@@ -1,5 +1,8 @@
 (ns sip.advent-of-code-2019)
 
+; https://adventofcode.com/2018
+
+; DAY 1
 (def input1
   (-> "src/sip/adv-input1.txt"
       (slurp)
@@ -24,6 +27,8 @@
          (reductions + 0)
          (find-non-unique #{})
          (take 10))))
+
+; DAY 2
 
 (def input2
   (-> "src/sip/adv-input2.txt"
@@ -68,4 +73,72 @@
        (map input2)
        (apply extract-similarity)
        (apply str))) 
-  
+
+; DAY 3
+
+; Line sample "#1 @ 265,241: 16x26"
+(defn parse-square [line]
+  (->> line
+       (re-seq #"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)")
+       (first)
+       (next)
+       (map #(Integer/parseInt %))
+       ((fn [[id x0 y0 dx dy]] {:id id, :a [x0 y0], :b [(+ x0 dx) (+ y0 dy)]}))))
+
+(def input3
+  (-> "src/sip/adv-input3.txt"
+      (slurp)
+      (clojure.string/split #"\n")
+      (#(mapv parse-square %))))
+
+
+(defn intersect-1-dim [a0 a1 b0 b1]
+  (cond 
+    (and (>= a0 b0) (< a0 b1)) [a0 (min a1 b1)]
+    (and (>= b0 a0) (< b0 a1)) [b0 (min a1 b1)]))
+
+(defn intersect-2-dim [[xa0 ya0] [xa1 ya1] [xb0 yb0] [xb1 yb1]]
+  (let [[x0 x1] (intersect-1-dim xa0 xa1 xb0 xb1)
+        [y0 y1] (intersect-1-dim ya0 ya1 yb0 yb1)]
+    (when (and x0 y0) 
+          [[x0 y0] [x1 y1]])))
+
+(defn make-couples [len]
+  (for [i (range len)
+        j (range (inc i) len)]
+    [i j]))    
+      
+(defn scatter-square [[x0 y0] [x1 y1]]
+  (for [i (range x0 x1)
+        j (range y0 y1)]
+    [i j]))
+
+(defn compare-elements [i j]
+  (let [{a0 :a, a1 :b} (input3 i)
+        {b0 :a, b1 :b} (input3 j)]
+    (intersect-2-dim a0 a1 b0 b1))) 
+
+(defn adv5[]
+  (->> (count input3)
+       (make-couples)
+       (map (partial apply compare-elements))
+       (filter some?)
+       (mapcat (partial apply scatter-square))
+       (distinct)
+       (count)))
+
+(defn has-no-intersections [i]
+  (every? (fn [[el j]] (or (= i j) (nil? (compare-elements i j))))
+          (map vector input3 (range))))
+
+(defn adv6[] 
+  (->> (count input3)
+       (range)
+       (filter has-no-intersections)
+       (map input3)))
+
+; DAY 4
+
+; [1518-10-23 23:47] Guard #1627 begins shift
+
+
