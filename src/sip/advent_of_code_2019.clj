109e4a26c844581590acda6c9e3fe3e21fd703bd
Maxim Fainberg <maksym.fainberg@mobenga.com>
2018-12-08 10:52:36 +0100

    Advent of code - day 6 - part 1 done

--- src/sip/advent_of_code_2019.clj
+++ src/sip/advent_of_code_2019.clj
@@ -232,3 +232,68 @@
        (map (comp count #(adv9 [] %)))
        (sort)
        (first)))
+
+;# DAY 6
+
+(def input6
+  (-> "src/sip/adv-input6.txt"
+      (slurp)
+      (clojure.string/split #"\n")
+      (->> 
+          (mapv (fn [line] (re-seq #"\d+" line)))
+          (mapv (fn [points] (map #(Integer/parseInt %) points))))))
+
+(defn get-limits [points]
+  (reduce (fn [{:keys [minx maxx miny maxy]} [x y]]
+            {:minx (if (< x minx) x minx)
+             :maxx (if (> x maxx) x maxx)
+             :miny (if (< y miny) y miny)
+             :maxy (if (> y maxy) y maxy)})
+           {:minx Integer/MAX_VALUE, :maxx 0, :miny Integer/MAX_VALUE, :maxy 0}
+           points))
+
+(defn get-min-with-ties [positive-ints] 
+  (->> positive-ints
+       (map vector (range))
+       (reduce (fn [[imin indexes] [index elem]]
+                 (cond
+                   (< elem imin) [elem [index]]
+                   (= elem imin) [elem (conj indexes index)]
+                   :else [imin indexes]))
+               [Integer/MAX_VALUE []])))
+
+(defn get-closest-index [[xa ya] points]
+  (->> points 
+       (map (fn [[x y]] (+ (Math/abs (- xa x)) (Math/abs (- ya y)))))
+       (get-min-with-ties)
+       ((fn [[imin indexes]] (if (= (count indexes) 1) (first indexes) -1))))) 
+
+(defn get-closest-map [points limits]
+  (for [x (range (:minx limits) (inc (:maxx limits)))
+        y (range (:miny limits) (inc (:maxy limits)))]
+    {:x x, :y y, :ci (get-closest-index [x y] points)}))    
+
+(defn get-ignorables [closest limits]
+  (->> closest
+      (filter 
+        (fn [{:keys [x y ci]}] 
+          (or (= x (:minx limits)) (= x (:xmax limits))
+              (= y (:miny limits)) (= y (:maxy limits)))))
+      (map :ci) 
+      (set)
+      (#(conj % -1))))
+
+(defn adv11
+  ([] (adv11 input6))
+  ([points] 
+    (let [limits (get-limits points)
+          closest (get-closest-map points limits)
+          ignorables (get-ignorables closest limits)]
+      (->> closest
+           (filter #(not (ignorables (:ci %)))) 
+           (map :ci)
+           (frequencies)
+           (apply max-key second)
+           (second)))))
+  
+  
