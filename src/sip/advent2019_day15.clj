a7fb401d05c48a147fe24a94784bd53056ac9482
Maxim Fainberg <maksym.fainberg@mobenga.com>
2019-01-05 18:10:25 +0100

    Advent of code - day 15 - data format

--- src/sip/advent2019_day15.clj
+++ src/sip/advent2019_day15.clj
@@ -1,8 +1,31 @@
 (ns sip.advent2019-day15)
 
-(def input15
-  (-> "src/sip/data/adv-input15.txt"
-      (slurp)
-      (clojure.string/split #"\n")))
+(defn initialize-position [{:keys [terrain persons] :as aa} [x y raw-value]]
+  (condp = raw-value
+    \# {:terrain (assoc-in terrain [y x] :wall), :persons persons}
+    \. {:terrain (assoc-in terrain [y x] :space), :persons persons}
+    \G (let [p {:id (count persons), :hp 300, :type :goblin, :pos [x y]}]
+         {:terrain (assoc-in terrain [y x] p), :persons (conj persons p)})
+    \E  (let [p {:id (count persons), :hp 300, :type :elf, :pos [x y]}]
+         {:terrain (assoc-in terrain [y x] p), :persons (conj persons p)})
+    (throw (Exception. "Unexpected object on the map"))))
 
+(defn get-initial-state [raw-terrain]
+  (let [maxy (count raw-terrain)
+        maxx (count (first raw-terrain))
+        empty-terrain (mapv (fn [row] (vec (repeat maxx :u)))
+                            (range maxy))]
+    (reduce initialize-position
+            {:terrain empty-terrain, :persons []}
+            (mapcat (fn [y] (map (fn [x] [x y (get-in raw-terrain [y x])])
+                                 (range maxx)))
+                    (range maxy)))))
+
+(def input
+  (-> "src/sip/data/adv-input15-small.txt"
+      (slurp)
+      (clojure.string/split #"\n")
+      (get-initial-state)))
+
+(defn part1 [] )
 
