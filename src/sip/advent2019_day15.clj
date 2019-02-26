e65717f15475b78d2a3cb004bbbcde92e3df2b5e
Maxim Fainberg <maksym.fainberg@mobenga.com>
2019-02-26 23:57:23 +0100

    Get back to track, document a bit

--- src/sip/advent2019_day15.clj
+++ src/sip/advent2019_day15.clj
@@ -1,6 +1,7 @@
 (ns sip.advent2019-day15)
 
-(defn initialize-position [{:keys [terrain creatures] :as aa} [x y raw-value]]
+; Put a wall, a space, a creature into terrain base on raw value
+(defn initialize-terrain-point [{:keys [terrain creatures]} [x y raw-value]]
   (condp = raw-value
     \# {:terrain (assoc-in terrain [y x] :wall), :creatures creatures}
     \. {:terrain (assoc-in terrain [y x] :space), :creatures creatures}
@@ -10,17 +11,20 @@
          {:terrain (assoc-in terrain [y x] creature), :creatures (conj creatures creature)})
     (throw (Exception. "Unexpected object on the map"))))
 
+; Convert two-dimensional raw terrain into State {:terrain :creatures}
 (defn get-initial-state [raw-terrain]
   (let [maxy (count raw-terrain)
         maxx (count (first raw-terrain))
         empty-terrain (mapv (fn [row] (vec (repeat maxx :u)))
                             (range maxy))]
-    (reduce initialize-position
+    (reduce initialize-terrain-point
             {:terrain empty-terrain, :creatures []}
             (mapcat (fn [y] (map (fn [x] [x y (get-in raw-terrain [y x])])
                                  (range maxx)))
                     (range maxy)))))
 
+; Get state
+; {:terrain [[] []], :creatures [{:id :hp :type :pos}] }
 (def input
   (-> "src/sip/data/adv-input15-small.txt"
       (slurp)
@@ -29,12 +33,15 @@
 
 (def directions [[0 -1] [-1 0] [1 0] [0 1]])
 
+; Find next alive creature starting from (current + 1) position and then circularly back to zero position  
 (defn find-next-creature-index [creatures current]
   (->> (range (inc current))
        (concat (range (inc current) (count creatures)))
        (filter (fn [i] (> (:hp (creatures i)) 0)))
        (first)))
 
+; Return new state after attack if it possible (a target in adjacent cell)
+; Otherwise return nil
 (defn try-attack [{:keys [terrain creatures current], :as state}]
   (let [{ctype :type, [x y] :pos} (creatures current)
         target (->> directions
@@ -54,6 +61,7 @@
           :current (find-next-creature-index creatures-after current)
           :last-moved current}))))
 
+
 (defn find-start-move-to-target [possible-moves]
   (->> possible-moves 
       (filter (comp :type :dest))
@@ -69,7 +77,9 @@
           (apply clojure.set/union (map (comp set :start-positions) grouped))]))
        (into {})))
 
-; {[x y] #{[x0 y0] [x0' y0']}
+; Input - {[x y] #{[x0 y0] [x0' y0']} - map key - a destination point, value - vector of starting positionss
+; [{:start-positions, :dest-pos, :dest}] 
+; TODO: fix keys
 (defn get-possible-moves [terrain enemy-type visited current-positions]
   (let [get-neigbour-moves (fn [x y start-positions]
     (->> directions 
@@ -82,7 +92,7 @@
          (keys)
          (mapcat (fn [[x y]] (get-neigbour-moves x y (current-positions [x y])))))))
 
-; state -> [x y]
+; Returning a starting point if enemy is reachable or nil otherwise
 (defn search-for-target [terrain enemy-type visited current-positions]
   (let [possible-moves (get-possible-moves terrain enemy-type visited current-positions)
         start-move (find-start-move-to-target possible-moves)]
@@ -92,6 +102,7 @@
             visited-next (apply conj visited (keys moves))]
           (recur terrain enemy-type visited-next moves)))))
 
+; Return possible moves from the point as {move #{move}}
 (defn get-initial-current-positions [terrain [x y]] 
   (->> directions
        (map (fn [[dx dy]]
@@ -100,6 +111,7 @@
        (filter (fn [{v :value}] (= v :space)))
        (map (fn [{p :pos}] [p #{p}]))
        (into {})))
+
 
 (defn move-creature [{:keys [terrain creatures current], :as state}]
   (let [{ctype :type, pos :pos} (creatures current)
@@ -110,13 +122,27 @@
 
 ;
 ; state keys - :terrain, :creatures, :current, :last-moved, :done
-(defn next-state [{:keys [last-moved current] :as state}]
+(defn next-state [{:keys [last-moved current terrain creatures] :as state}]
   (if (= last-moved current) 
     (assoc state :done true)
     (if-let [state-after (try-attack state)]
       state-after
-      (let [move (move-creature state)]
+      (let [move (move-creature state)
+        {pos-b :pos, :as creature} (creatures current)
+        creature-after (assoc creature :pos move)
+        creatures-after (assoc creatures current creature-after)
+        terrain-after (assoc-in terrain (reverse pos-b) :space)
+        terrain-after (assoc-in terrain-after (reverse move) creature-after)]
+        {:terrain terrain-after 
+         :creatures creatures-after
+         }
         move))))
+; change pos in creature
+; terrain: set :space to old pos, put creature into new pos
+; change creature
+; change last-moved
+; if no move set done
+
 
 (defn part1
   ([] (part1 (assoc input :current 0)))
